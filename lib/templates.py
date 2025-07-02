6bac4a8549946b2c7e29a0d5b383da224cb39bfe
Sergio Delgado Quintero <sdelquin@gmail.com>
2025-07-02 19:32:34 +0100

    Improve aesthetics on publication results

--- lib/templates.py
+++ lib/templates.py
@@ -5,12 +5,30 @@ from jinja2 import Environment, FileSystemLoader
 
 import settings
 
+
+def normalize(value):
+    if value is None:
+        return settings.NONE_REPR
+    if not isinstance(value, str):
+        return value
+    value = value.strip()
+    if value == 'APTO':
+        return f'<span style="color: blue;">{value}</span>'
+    try:
+        grade = float(value)
+    except (ValueError, TypeError):
+        return value
+    else:
+        color = 'green' if grade >= 5 else 'red'
+    return f'<span style="color: {color};">{grade}</span>'
+
+
 env = Environment(loader=FileSystemLoader(settings.TEMPLATES_DIR))
 
 env.globals['hash'] = hashlib.sha256(datetime.now().isoformat().encode()).hexdigest()
 env.globals['hero_emoji'] = settings.HERO_EMOJI
 
-env.filters['none_to_empty'] = lambda value: value if value is not None else settings.NONE_REPR
+env.filters['normalize'] = normalize
 
 
 def render_template(template_path, **context):
