9de0b841b40c422327a69f331fd876bdbbf2b5ae
Sergio Delgado Quintero <sdelquin@gmail.com>
2025-07-08 19:41:59 +0100

    Add some plots

--- eda/primera-prueba.R
+++ eda/primera-prueba.R
@@ -1,7 +1,11 @@
 library(tidyverse)
+library(ggdist)
 
 DATA_PATH <- "../data/primera-prueba/"
 
+# ==============================================================================
+# Carga de datos
+# ==============================================================================
 df <- list.files(path = DATA_PATH, pattern = "\\.csv$", full.names = T) |>
   set_names() |>
   map(\(path) read_csv(
@@ -9,6 +13,16 @@ df <- list.files(path = DATA_PATH, pattern = "\\.csv$", full.names = T) |>
     na = c("-", "No presentado", "Excluido")
   )) |>
   bind_rows(.id = "id") |>
+  mutate(
+    `PARTE A` = case_when(
+      Especialidad %in% c("Peluquería", "Dibujo") ~ round((Prueba - 0.4 * `PARTE B`) / 0.6, 4),
+      TRUE ~ `PARTE A`
+    ),
+    ...12 = case_when(
+      Especialidad %in% c("Peluquería", "Dibujo") ~ ...13,
+      TRUE ~ ...12
+    )
+  ) |>
   transmute(
     proceso = as_factor(Proceso),
     cuerpo = as_factor(Cuerpo),
@@ -23,35 +37,121 @@ df <- list.files(path = DATA_PATH, pattern = "\\.csv$", full.names = T) |>
     nota = Prueba,
     resultado = as_factor(`...12`)
   ) |>
-  # Dibujo y Peluquería presentan una estructura de columnas distintas
-  filter(!especialidad %in% c("Dibujo", "Peluquería"))
-
-df_dibpel <- df |>
-  filter(Especialidad %in% c("Dibujo", "Peluquería"))
-
-df |>
-  filter(nota == 10) |>
-  t() |>
-  View()
+  mutate(
+    especialidad_c = as_factor(case_when(
+      cuerpo == "Cuerpo de Maestros" ~ paste(especialidad, "(P)"),
+      TRUE ~ paste(especialidad, "(S)")
+    ))
+  ) |>
+  relocate(especialidad_c, .after = especialidad)
 
 df |>
-  group_by(especialidad) |>
+  write_csv('primera_prueba.csv')
+
+# ==============================================================================
+# Nota media de la primera prueba por especialidad
+# ==============================================================================
+df |>
+  group_by(especialidad_c) |>
   summarize(
     nota = mean(nota, na.rm = T)
   ) |>
-  ggplot(aes(x = fct_reorder(especialidad, desc(nota)), y = nota, fill = nota)) +
+  ggplot(aes(x = fct_reorder(especialidad_c, nota), y = nota, fill = nota)) +
     geom_col() +
-    geom_text(aes(label = round(nota, 2)), vjust = -0.3, size = 3) +
+    coord_flip() +
+    geom_label(aes(label = sprintf("%.02f", nota)), size = 2.5, hjust = 1.2, fill = "white", alpha = 0.6) +
     scale_fill_viridis_c() +
-    theme_light() +
+    scale_y_continuous(expand = expansion(mult = c(0, 0.1))) +
     labs(
       title = "Nota media de la primera prueba por especialidad",
       subtitle = "Oposiciones del profesorado 2025",
-      caption = "Datos publicados por la Consejería de Educación del Gobierno de Canarias",
+      caption = paste(
+        "* (P) = Primaria; (S) = Secundaria",
+        "* No se están teniendo en cuenta aspirantes no presentados o excluidos",
+        "© Sergio Delgado Quintero | Datos publicados por la Consejería de Educación del Gobierno de Canarias",
+        sep = "\n"
+      ),
       x = NULL, y = NULL
     ) +
-    expand_limits(y = max(df$nota) * 1.5) +
     guides(fill = "none") +
+    theme_minimal(base_family = "Roboto") +
     theme(
-      axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)
+      panel.grid = element_blank(),
+      plot.margin = margin(t = 40, r = 20, b = 40, l = 20),
+      axis.text.x = element_blank(),
+      plot.title = element_text(size = 16, face = "bold", color = "gray20"),
+      plot.subtitle = element_text(size = 14, color = "gray30", margin = margin(b = 20)),
+      plot.caption = element_text(margin = margin(t = 30), color = "gray40")
+    )
+
+# ==============================================================================
+# Porcentaje aptos de la primera prueba por especialidad
+# ==============================================================================
+df |>
+  drop_na() |>
+  group_by(especialidad_c) |>
+  count(resultado) |>
+  mutate(
+    p = n / sum(n) * 100,
+  ) |>
+  filter(resultado == "APTO") |>
+  ggplot(aes(x = fct_reorder(especialidad_c, p), y = p, fill = p)) +
+    geom_col() +
+    coord_flip() +
+    geom_label(aes(label = sprintf("%.01f%%", p)), size = 2.5, hjust = 1.2, fill = "white", alpha = 0.6) +
+    scale_fill_viridis_c() +
+    scale_y_continuous(expand = expansion(mult = c(0, 0.1))) +
+    labs(
+      title = "Porcentaje aptos de la primera prueba por especialidad",
+      subtitle = "Oposiciones del profesorado 2025",
+      caption = paste(
+        "* (P) = Primaria; (S) = Secundaria",
+        "* No se están teniendo en cuenta aspirantes no presentados o excluidos",
+        "© Sergio Delgado Quintero | Datos publicados por la Consejería de Educación del Gobierno de Canarias",
+        sep = "\n"
+      ),
+      x = NULL, y = NULL
+    ) +
+    guides(fill = "none") +
+    theme_minimal(base_family = "Roboto") +
+    theme(
+      panel.grid = element_blank(),
+      plot.margin = margin(t = 40, r = 20, b = 40, l = 20),
+      axis.text.x = element_blank(),
+      plot.title = element_text(size = 16, face = "bold", color = "gray20"),
+      plot.subtitle = element_text(size = 14, color = "gray30", margin = margin(b = 20)),
+      plot.caption = element_text(margin = margin(t = 30), color = "gray40")
+    )
+
+# ==============================================================================
+# Distribución de notas (densidad) de la primera prueba por especialidad
+# ==============================================================================
+df |>
+  drop_na() |>
+  mutate(
+    especialidad_c = fct_reorder(especialidad_c, as.character(especialidad_c))
+  ) |>
+  ggplot(aes(x = nota)) +
+    geom_density(fill = "skyblue", color = "steelblue4") +
+    facet_wrap(~especialidad_c) +
+    labs(
+      title = "Distribución de notas (densidad) de la primera prueba por especialidad",
+      subtitle = "Oposiciones del profesorado 2025",
+      caption = paste(
+        "* (P) = Primaria; (S) = Secundaria",
+        "* No se están teniendo en cuenta aspirantes no presentados o excluidos",
+        "© Sergio Delgado Quintero | Datos publicados por la Consejería de Educación del Gobierno de Canarias",
+        sep = "\n"
+      ),
+      x = NULL, y = NULL
+    ) +
+    theme_ggdist(base_family = "Roboto") +
+    theme(
+      panel.grid = element_blank(),
+      strip.background = element_rect(fill = "skyblue4"),
+      strip.text = element_text(color = "white", face = "bold"),
+      plot.margin = margin(t = 40, r = 20, b = 40, l = 20),
+      plot.title = element_text(size = 16, face = "bold", color = "gray20"),
+      plot.subtitle = element_text(size = 14, color = "gray30", margin = margin(b = 20)),
+      plot.caption = element_text(margin = margin(t = 30), color = "gray40")
     )
