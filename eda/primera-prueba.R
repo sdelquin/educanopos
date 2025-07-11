24797b935ab98c7e72bf314064559dbff7b81048
Sergio Delgado Quintero <sdelquin@gmail.com>
2025-07-11 19:37:04 +0100

    Add more plots for primera prueba

--- eda/primera-prueba.R
+++ eda/primera-prueba.R
@@ -346,3 +346,39 @@ df |>
       axis.title.y = element_text(margin = margin(r = 20)),
       legend.title = element_blank()
     )
+
+# ==============================================================================
+# Porcentaje no presentados de la primera prueba por especialidad
+# ==============================================================================
+df |>
+  group_by(especialidad_c) |>
+  summarize(
+    ratio_np = sum(np) / n() * 100,
+  ) |>
+  arrange(desc(ratio_np)) |>
+  ggplot(aes(x = fct_reorder(especialidad_c, ratio_np), y = ratio_np, fill = ratio_np)) +
+    geom_col() +
+    coord_flip() +
+    geom_label(aes(label = sprintf("%.01f%%", ratio_np)), size = 2.5, hjust = 1.2, fill = "white", alpha = 0.6) +
+    scale_fill_viridis_c() +
+    scale_y_continuous(expand = expansion(mult = c(0, 0.1))) +
+    labs(
+      title = "Porcentaje no presentados de la primera prueba por especialidad",
+      subtitle = "Oposiciones del profesorado 2025",
+      caption = paste(
+        "* (P) = Primaria; (S) = Secundaria",
+        "© Sergio Delgado Quintero | Datos publicados por la Consejería de Educación del Gobierno de Canarias",
+        sep = "\n"
+      ),
+      x = NULL, y = NULL
+    ) +
+    guides(fill = "none") +
+    theme_minimal(base_family = "Roboto") +
+    theme(
+      panel.grid = element_blank(),
+      plot.margin = margin(t = 40, r = 20, b = 40, l = 20),
+      axis.text.x = element_blank(),
+      plot.title = element_text(size = 16, face = "bold", color = "gray20"),
+      plot.subtitle = element_text(size = 14, color = "gray30", margin = margin(b = 20)),
+      plot.caption = element_text(margin = margin(t = 30), color = "gray40")
+    )
